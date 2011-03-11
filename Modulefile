c1e9c60a7e0771f85ca2b27b2284bc1d56714d5b
Jeff McCune <jeff@puppetlabs.com>
2011-03-11 14:28:09 -0800

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'jeffmccune-motd'
+version '1.0.0'
+source 'puppetlabs-motd
+author 'Jeff McCune <jeff@puppetlabs.com>
+license 'Apache'
+summary 'Manages the message of the day'
+description 'Places information about the host in the message of the day.'
+project_page 'https://github.com/jeffmccune/jeffmccune-motd'
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+# Class: motd
+#
+# This module manages the /etc/motd file using a template
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#  include motd
+#
+# [Remember: No empty lines between comments and class definition]
+class motd {
+  file { '/etc/motd':
+    ensure  => file,
+    content => template("${module_name}/motd.erb"),
+    owner   => 0,
+    group   => 0,
+    mode    => 0644,
+  }
+}
